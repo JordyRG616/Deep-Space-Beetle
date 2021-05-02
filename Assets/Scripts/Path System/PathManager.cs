5c6125ab5946f13910ea98d4f44370ff5ebc1c3a
Jordy_Rodrigues <jordyrg616@gmail.com>
2021-05-02 16:45:16 -0300

    Setup inicial do scrip PathManager;

---
+++ Assets/Scripts/Path System/PathManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathManager : MonoBehaviour
+{
+    [SerializeField] private BeetleMovement beetle;
+    private GameObject activePath, nextPath;
+
+    
+}
