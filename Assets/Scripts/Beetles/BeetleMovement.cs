f76f4fa1b349b4249b0f592b8fc8e869ed58757e
Jordy_Rodrigues <jordyrg616@gmail.com>
2021-05-02 16:37:47 -0300

    Adicionado movimento automático com velocidade variando com o tamanho do caminho;

--- Assets/Scripts/Beetles/BeetleMovement.cs
+++ Assets/Scripts/Beetles/BeetleMovement.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 public class BeetleMovement : MonoBehaviour
 {
     [SerializeField] private GameObject path;
-    [SerializeField][Range(0, 1f)] private float elapsedTime;
+    private float elapsedTime;
+    [SerializeField] private float totalTime = 1.5f;
     private List<Transform> nodes = new List<Transform>();
 
     private void Awake()
@@ -16,18 +17,39 @@ public class BeetleMovement : MonoBehaviour
 
         transform.position = nodes[0].position;
     }
-
-    private void LerpMove(Transform node1, Transform node2, Transform node3)
+    
+    private void Start()
     {
-        Vector3 lerpNodes12 = Vector3.Lerp(node1.position, node2.position, elapsedTime);
-        Vector3 lerpNodes23 = Vector3.Lerp(node2.position, node3.position, elapsedTime);
+        StartCoroutine(LerpMove(nodes[0], nodes[1], nodes[2]));
+    }
 
-        Vector3 lerpTotal = Vector3.Lerp(lerpNodes12, lerpNodes23, elapsedTime);
-        Vector3 nextlerp = Vector3.LerpUnclamped(lerpNodes12, lerpNodes23, elapsedTime + 0.1f);
+    private IEnumerator LerpMove(Transform node1, Transform node2, Transform node3)
+    {
+        float velocity = CalculateVelocity(node1.position, node3.position);
 
-        transform.position = lerpTotal;
+        while(elapsedTime < totalTime)
+        {
+            Vector3 lerpNodes12 = Vector3.Lerp(node1.position, node2.position, elapsedTime * velocity);
+            Vector3 lerpNodes23 = Vector3.Lerp(node2.position, node3.position, elapsedTime * velocity);
 
-        AdjustRotation(transform.position, nextlerp);
+            Vector3 lerpTotal = Vector3.Lerp(lerpNodes12, lerpNodes23, elapsedTime * velocity);
+            Vector3 nextlerp = Vector3.LerpUnclamped(lerpNodes12, lerpNodes23, (elapsedTime *velocity) + 0.1f);
+
+            transform.position = lerpTotal;
+
+            AdjustRotation(transform.position, nextlerp);
+
+            yield return new WaitForSecondsRealtime(0.1f);
+            elapsedTime += 0.1f;
+        }
+
+            elapsedTime = 0f;
+    }
+
+    private float CalculateVelocity(Vector3 startPoint, Vector3 endPoint)
+    {
+        float distance = (startPoint - endPoint).magnitude;
+        return distance / totalTime;
     }
 
     private void AdjustRotation(Vector3 pos, Vector3 posmaisDpos)
@@ -39,8 +61,8 @@ public class BeetleMovement : MonoBehaviour
         transform.rotation = Quaternion.Euler(0, 0, angle - 90f); 
     }
 
-    private void Update()
-    {
-        LerpMove(nodes[0], nodes[1], nodes[2]);
-    }
+    //private void Update()
+    //{
+    //    LerpMove(nodes[0], nodes[1], nodes[2]);
+    //}
 }
